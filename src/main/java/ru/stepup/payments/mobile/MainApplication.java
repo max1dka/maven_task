62cd09a20f175b8cd0b4316467707c6a90f98877
max1dka <mitrofanov.m00@icloud.com>
2025-11-25 16:03:10 +0300

    make task with maven + new name program

--- src/main/java/ru/stepup/payments/mobile/MainApplication.java
+++ src/main/java/ru/stepup/payments/mobile/MainApplication.java
@@ -2,6 +2,6 @@ package ru.stepup.payments.mobile;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Welcome to Mobile 111");
+        System.out.println("Welcome to Mobile Sonya");
     }
 }
